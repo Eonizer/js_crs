27f63a356d858c8a17e3d5b33a0c8d82a86bab1f
Eonizer <lollips911@gmail.com>
2022-03-17 23:22:28 +0300

    added ch13 - weather app

---
+++ weather_app/scripts/forecast.js
@@ -0,0 +1,23 @@
+const key = 'puZjPTHEN4Hpbdyk1d4jqtkUXI6OoTiH';
+
+//get weather inmfo
+async function getWeather(id){
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base+query);
+    const data = await response.json();
+
+    return data[0]; 
+}
+
+//get city info
+async function getCity(city){
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base+query);
+    const data = await response.json();
+
+    return data[0];
+};
