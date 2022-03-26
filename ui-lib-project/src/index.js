09a0c77c9ab50427efea6440c9b699b73313265c
Eonizer <lollips911@gmail.com>
2022-03-27 01:37:22 +0300

    added snack to UI-LIB

--- ui-lib-project/src/index.js
+++ ui-lib-project/src/index.js
@@ -1,5 +1,7 @@
 import Tooltip from './ninja-ui/tooltip';
 import Dropdown from './ninja-ui/dropdown';
+import Tabs from './ninja-ui/tabs';
+import Snackbar from './ninja-ui/snackbar';
 
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 tooltip.init();
@@ -11,3 +13,16 @@ dropdowns.forEach(dropdown => {
     const instance = new Dropdown(dropdown);
     instance.init();
 })
+
+//create tabs 
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
+
+//create snack
+const snackbar = new Snackbar();
+snackbar.init();
+
+const buttom = document.querySelector('button');
+buttom.addEventListener('click', ()=>{
+    snackbar.show('Hey :)');
+});
